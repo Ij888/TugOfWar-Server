1e5497e83166019dc480a79fed5969cc8cae04e0
Don Turner <doturner@rim.com>
2013-03-06 21:59:48 +0000

    Moved client.js inside index.html, some cosmetic changes

--- client.js
+++
@@ -1,5 +0,0 @@
-var socket =io.connect('http://localhost:8082');
-function pull(direction){
-	socket.emit('pull', direction);
-	console.log('Received pull '+direction);
-}
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 var io = require('socket.io').listen(8082);
-var socketid;
+var app_socket_id;
 
 io.sockets.on('connection', function (socket) {
+  
   socket.emit('ready', 'Server ready');
+  
+  //Listen for app to connect
   socket.on('itsamemario', function (data) {
-        socketid = socket.id;
-    });
-  socket.on('pull', function (data) {
-	if(socketid != null){
-		io.sockets.socket(socketid).emit('rope', data);	
-		console.log('client is connected');
-	}else{
-		console.error('client is not connected yet');
-	}
-    });
+    app_socket_id = socket.id;
+    console.log('client app connected');
   });
+  
+  //Listen for players sending pull rope signals
+  socket.on('pull', function (data) {
+    if(app_socket_id != null){
+		  io.sockets.socket(app_socket_id).emit('rope', data);	
+		  console.log('client app is connected');
+    }else{
+		  console.error('client app is not connected yet');
+    }
+  });
+});
